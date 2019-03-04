a7698dffbae2c969e8dfa98a7d3ccc0d3b2f284b
Cristian <cristianlistasseguras@gmail.com>
2019-03-03 20:25:40 -0500

    release from Android - Termux

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const router = express.Router();
+
+const port = Number(3000)
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hola, Mundo NodeJs - Android') 
+})
+
+app.listen(port, () => {
+   console.log(`Server Runing in port ${port}`)
+})
+
+router.get('/users', (req, res) => {
+   res.json({
+      users: []
+   })
+})
+
+router.get('/', function(req, res){
+ res.sendStatus(200)
+})
+
+app.use('/api', router);
